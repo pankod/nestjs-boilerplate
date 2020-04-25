0ce3f8db2087fa818b88e3d765bb1bd7b5111a2d
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2020-04-25 12:05:29 +0300

    e2e setup

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,34 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 
-// modules
-import { AppModule } from './../src/app.module';
-import { ConfigModule } from './../src/config/config.module';
-
-// factories
-import { TypeOrmConfigService } from './factories/database.factory';
-
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [
-        ConfigModule,
-        TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          useClass: TypeOrmConfigService,
-        }),
-        AppModule,
-      ],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(global.app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
